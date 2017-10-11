25cc62a45016d932d38fd8d9152ee8985f4f181c
Sean Markert <markertsean0@gmail.com>
2017-10-11 15:46:09 -0700

    Added a data cleaning page

--- read_quote.py
+++ read_quote.py
@@ -15,7 +15,12 @@ def fill_quote_gaps( inp_df ):
 def fix_splits( inp_df ):
     
     # Possible values for splits
-    split_list = [ 1/5., 1/4., 1/3., 1/2., 2., 3., 4., 5. ]
+    split_list = [ 
+                   1/5., 1/4., 1/3., 1/2., 2/1., 3/1., 4/1., 5/1., 
+                   2/5.,       2/3.,             3/2.,       5/2., 
+                   3/5., 3/4.,                         4/3., 5/3., 
+                   4/5.,                                   , 5/4.                                                                   
+                  ]
     
     while True:
     
